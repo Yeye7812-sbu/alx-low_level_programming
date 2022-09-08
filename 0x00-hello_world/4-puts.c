fa6e985def1d131511d9487fb7ec9f272ed023f2
Sibongile Simbeye <sibongilesimbeyejy@gmail.com>
2022-09-08 16:58:12 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
- /**
-     * main -Entry point
-     * Return: 0 if successful
-  */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */     
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
