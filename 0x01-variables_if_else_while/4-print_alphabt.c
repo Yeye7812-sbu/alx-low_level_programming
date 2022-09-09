96e4d8dc59da22787d38d0a6e576810790b4b58b
Sibongile Simbeye <sibongilesimbeyejy@gmail.com>
2022-09-09 11:37:29 -0700

    The alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Description: Use putchar to print all letters but the letter 'q' and 'e'.
- * Return: Always 0 if exited properly
+ * Description: Use `putchar` to print all letters but the letter 'q' and 'e'
+ * Return: Aways 0 if exited successfully
  */
 int main(void)
 {
-	char c = 'a';
+	char ch = 'a';
 
-	while (c <= 'z')
+	while (ch <= 'z')
 	{
-		if (c != 'q' && c != 'e')
-			putchar(c);
-		c++;
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
 	}
 	putchar('\n');
-
 	return (0);
 }
-Footer
-
