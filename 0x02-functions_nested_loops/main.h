3bc5ec66a5cbab0729a8650a97b4cfb90eb2a764
Sibongile Simbeye <sibongilesimbeyejy@gmail.com>
2022-09-13 17:46:54 -0700

    World multiplication-table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -19,10 +19,14 @@ int print_last_digit(int);
 
 void jack_bauer(void);
 
-void print_times_table(int i, int j, int k);
+void print_times_table(int n);
 
 int add(int a, int b);
 
 void print_to_98(int n);
+
+void print_times_table(int n);
+
+
 
 #endif
