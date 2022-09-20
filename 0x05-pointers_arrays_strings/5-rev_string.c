26b053f9d60ab9fee1569e8f27eda984f293baa4
Sibongile Simbeye <sibongilesimbeyejy@gmail.com>
2022-09-19 18:18:12 -0700

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,16 +8,15 @@
 * Return: nothing
 */
 
-void rev_string(char *k)
+void rev_string(char *s)
 {
+	int i, tmp, len = _strlen(s);
 
-	int j, tmp, len = _strlen(s);
-
-	for (i = 0; j < len / 2; j++)
+	for (i = 0; i < len / 2; i++)
 	{
-		tmp = *(k + j);
-		*(k + j) = *(k + len - j - 1);
-		*(k + len - j - 1) = tmp;
+		tmp = *(s + i);
+		*(s + i) = *(s + len - i - 1);
+		*(s + len - i - 1) = tmp;
 	}
 }
 
@@ -28,11 +27,11 @@ void rev_string(char *k)
 * Return: the length of the given string
 */
 
-int _strlen(char *k)
+int _strlen(char *s)
 {
 	int len = 0;
 
-	while (*(k + len) != '\0')
+	while (*(s + len) != '\0')
 		len++;
 
 	return (len);
