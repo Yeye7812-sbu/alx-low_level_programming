e7d4cbb0a4089f27e9e9e6b21bacb7498f1e4f6f
Sibongile Simbeye <sibongilesimbeyejy@gmail.com>
2022-09-28 17:34:07 -0700

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,7 +13,7 @@ int is_prime_number(int n)
 
 	if (n <= 1)
 		return (0);
-	return (prime(n, start));
+	return (is_prime(n, start));
 }
 
 /**
@@ -24,11 +24,11 @@ int is_prime_number(int n)
  * Return: 1 if n is prime, 0 otherwise
  */
 
-int prime(int n, int start)
+int is_prime(int n, int start)
 {
 	if (start <= 1)
 		return (1);
 	else if (n % start == 0)
 		return (0);
-	return (prime(n, start - 1));
+	return (is_prime(n, start - 1));
 }
