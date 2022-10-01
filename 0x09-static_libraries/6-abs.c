902cf818b99fc7faac780e6c6a4d5bd65a67ebac
Yeye7812-sbu <sibongilesimbeyejy@gmail.com>
2022-09-30 18:51:47 -0700

    task 0

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @j: the int to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int j)
+{
+	if (j >= 0)
+	{
+		return (j);
+	}
+	return (-j);
+}
